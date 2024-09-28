7c606a76b6b1fad076f55ff22d0f8bf257ec12ef
TharushaKula <tharusha22589@gmail.com>
2024-09-28 21:43:07 +0530

    Refactor Clothes schema to add references for size and color

--- src/infrastructure/schemas/Clothes.js
+++ src/infrastructure/schemas/Clothes.js
@@ -1,34 +1,34 @@
 import mongoose from "mongoose";
 
 const ClothesSchema = new mongoose.Schema({
-    categoryId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: String,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    size: {
-        type: String,
-        required: true,
-    },
-    color: {
-        type: String,
-        required: true,
-    },
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Sizes",
+  },
+  color: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Colors",
+  },
 });
 
 const Clothes = mongoose.model("Clothes", ClothesSchema);
